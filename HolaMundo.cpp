6b561eed4b8c437c05c31cd9e35dc7dcdfc9c1ac
Pablo Uribe <pablo.um00@gmail.com>
2023-04-16 12:32:56 -0500

    Initial commit

---
+++ HolaMundo.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+//using namespace std; 
+
+int main(){
+
+    std::cout << "Hola mundo desde mi nueva mac " << std::endl;
+    std::cout << "Hallo ich bin Santiago" << std::endl;
+    std::cout << "Ciao, Camilla" << std::endl;
+
+    return 0;
+}
